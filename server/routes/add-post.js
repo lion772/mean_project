913639f886008f518d4004e9ea51300183f5422a
william <william.steinkedemello@gmail.com>
2022-12-19 13:16:01 +0100

    server side upload added

--- server/routes/add-post.js
+++ server/routes/add-post.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../model");
+const multer = require("multer");
 const { insertPost } = require("../../databasepg");
 
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    /* MIME_TYPE_MAP[file.mimetype]
+      ? cb(null, "./images")
+      : cb(new Error("Invalid mime type"), "./images"); */
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "./images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, `${name} - ${Date.now()}.${ext}`);
+  },
+});
+
 //MongoDB
-router.post("/", function (req, res) {
+router.post("/", multer(storage).single("image"), (req, res) => {
   console.log("body is ", req.body);
   let addPost = new Post({
     title: req.body.title,
